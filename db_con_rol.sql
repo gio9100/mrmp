aa0a15ba5353f5a846843e4c424c24dc10410e51
giovanni52 <giovannivillanueva23@cbtis52.edu.mx>
2025-10-21 16:58:32 -0600

    Delete db_con_rol.sql

--- db_con_rol.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(120) NOT NULL UNIQUE,
-    contrasena_hash VARCHAR(255) NOT NULL,
-    rol ENUM('user','admin') DEFAULT 'user',
-    clave_admin VARCHAR(255) NULL
-);
