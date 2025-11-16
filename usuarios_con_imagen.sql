80197091a0f83b53d46093ecd590cb583626f67e
giovanni52 <giovannivillanueva23@cbtis52.edu.mx>
2025-11-15 21:40:25 -0600

    base de datos actualizada, con token para recuperar la contraseña

--- usuarios_con_imagen.sql
+++ usuarios_con_imagen.sql
@@ -1,10 +1,14 @@
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) NOT NULL UNIQUE,
-    contrasena_hash VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) DEFAULT NULL,
-    imagen_perfil VARCHAR(255) DEFAULT 'default.png',
-    verificado TINYINT(1) DEFAULT 0,
-    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contrasena_hash` varchar(255) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `imagen_perfil` varchar(255) DEFAULT 'default.png',
+  `verificado` tinyint(1) DEFAULT 0,
+  `fecha_creacion` datetime DEFAULT current_timestamp(),
+  `reset_token` varchar(100) DEFAULT NULL,
+  `token_expirada` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
